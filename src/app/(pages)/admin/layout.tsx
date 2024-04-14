6770a9db1112f902a4dad6fe7fb63bb67c05c6ca
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2024-04-14 11:13:53 +0200

    Changed file names

--- src/app/(pages)/admin/layout.tsx
+++ src/app/(pages)/admin/layout.tsx
@@ -1,20 +1,14 @@
-import AdminNavbar from "./components/navbar/AdminNavbar";
-import AdminSidebar from "./components/sidebar/adminSidebar";
+import AdminNavbar from './components/navbar/adminNavbar';
+import AdminSidebar from './components/sidebar/adminSidebar';
 
-
-export default function AdminLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <nav className="fixed z-50">
-        <AdminNavbar/>
-        <AdminSidebar/>
+        <AdminNavbar />
+        <AdminSidebar />
       </nav>
-        <main className="pl-[13.5rem] pt-[4.5rem] pb-6 pr-6">{children}</main>
+      <main className="pl-[13.5rem] pt-[4.5rem] pb-6 pr-6">{children}</main>
     </>
   );
 }
